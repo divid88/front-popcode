cd1fe1ad1e4f8e794b47d26cfdc91db97501d434
david <divid28p@gmail.com>
2024-12-21 21:53:20 +0400

    second commit

--- app/tutorial/components/EditorCode.tsx
+++ app/tutorial/components/EditorCode.tsx
@@ -15,10 +15,17 @@ import Output from './Output';
 
 function EditorCode() {
   const [value, setValue] = React.useState<string>(`x = "david"\ny = "John"\nprint(x)\nprint(y)`)
+  const [valueMobile, setValueMobile] = React.useState<string>(`x = "david"\ny = "John"\nprint(x)\nprint(y)`)
   const editorRef = useRef(null);
+  const editorRefMobile = useRef(null)
 
   const onMount = (editor: any) => {
     editorRef.current = editor
+    editor.focus()
+  }
+
+  const onMountMobile = (editor: any) => {
+    editorRefMobile.current = editor
     editor.focus()
   }
 
@@ -30,13 +37,14 @@ function EditorCode() {
     <AccordionTrigger className='bg-primary  rounded-lg px-7 py-1'>  تمرین کد زنی.... </AccordionTrigger>
     <AccordionContent>
 
-    <div className=' grid grid-cols-12 bg-secondary w-full p-2 rounded-lg ' 
+    <div className='hidden  md:grid grid-cols-12 bg-secondary w-full p-2 
+                    rounded-lg ' 
           dir='ltr'>
-      <div className='col-span-6'>
+      <div className=' md:col-span-6'>
         <Editor
-          height="44.5vh" 
-          width="75vh"
-          className='font-thin leading-4 p-0  text-xs'
+          height="45.5vh" 
+          width="73vh"
+          className='font-thin leading-4 p-0   text-xs'
           defaultLanguage="python"
           defaultValue="// some comment"
           value={value}
@@ -46,8 +54,31 @@ function EditorCode() {
         />
       </div>
 
-      <div className=' col-span-6'>
+      <div className=' col-span-12 md:col-span-6'>
         <Output editorRef={editorRef}/>
+      </div>
+
+    </div>
+
+    <div className=' block md:hidden grid-cols-12 bg-secondary w-full p-2 
+                    rounded-lg ' 
+          dir='ltr'>
+      <div className=' md:col-span-11 overflow-hidden'>
+        <Editor
+          height="20.5vh" 
+          width="50vh"
+          className='font-thin leading-4 p-0   text-xs'
+          defaultLanguage="python"
+          defaultValue="// some comment"
+          value={valueMobile}
+          theme='vs-dark'
+          onMount={onMountMobile}
+
+        />
+      </div>
+
+      <div className=' col-span-12'>
+        <Output editorRef={ editorRefMobile}/>
       </div>
 
     </div>
